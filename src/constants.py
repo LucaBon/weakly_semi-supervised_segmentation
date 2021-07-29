f2c2ec03749d018851c8b9719a4e8fc93e7a5ec3
Bonura, Luca <luca.bonura@ags-it.com>
2021-07-29 16:56:57 +0200

    add model

---
+++ src/constants.py
@@ -0,0 +1,55 @@
+import torch
+
+TILE_SIZE = (200, 200)
+IMAGE_FOLDER_PATH = "/images"
+LABELS_FOLDER_PATH = "/labels"
+
+NAME_FORMAT = "top_mosaic_09cm_area{}.tif"
+
+INPUT_CHANNELS = 3
+BATCH_SIZE = 10
+N_CLASSES = 6
+
+# car class is weighted more
+PIXEL_WEIGHTS = torch.FloatTensor([1.0, 1.0, 1.0, 1.0, 10.0, 1.0])
+
+N_CLASSES_IMAGE_LABELS = 5  # clutter is neglected
+# car class is weighted more
+IMAGE_WEIGHTS = torch.FloatTensor([1.0, 1.0, 1.0, 1.0, 10.0])
+
+CACHE = True
+
+# Threshold in percentage ( # pixels_of_that_class / # all_pixels)
+# If the percentage of pixel of a specific class is greater than the threshold
+# that class is considered present, otherwise it is considered absent
+THRESHOLD_IMAGE_LABELS = 1
+
+# if the ground truth y_true is [1,0,0,1,1], while the predicted output
+# vector y_hat is [0.7, 0.3, 0.5, 0.6, 0.8], to evaluate performances a
+# threshold value is picked for the predicted labels to be accepted as true
+MULTI_LABEL_THRESHOLD = 0.5
+
+LABEL_NAMES = ["Impervious surfaces",
+               "Building",
+               "Low vegetation",
+               "Tree",
+               "Car",
+               "Clutter"]
+
+COLOR_MAPPING = {0: (255, 255, 255),  # Impervious surfaces (WHITE)
+                 1: (0, 0, 255),  # Building (BLUE)
+                 2: (0, 255, 255),  # Low vegetation (TURQUOISE)
+                 3: (0, 255, 0),  # Tree (GREEN)
+                 4: (255, 255, 0),  # Car (YELLOW)
+                 5: (255, 0, 0),  # Clutter/background (RED)
+                 }
+
+INVERSE_COLOR_MAPPING = {v: k for k, v in COLOR_MAPPING.items()}
+
+# Number of epoch performed using just image-level labels
+EPOCHS_IMAGE_LABELS = 3
+LR_IMAGE_LABELS = 0.002
+# Number of epochs performed using pixel level labels
+EPOCHS = 20
+
+VGG16_URL = 'https://download.pytorch.org/models/vgg16_bn-6c64b313.pth'
---
+++ src/model.py
@@ -0,0 +1,171 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from constants import INPUT_CHANNELS,\
+    N_CLASSES, \
+    N_CLASSES_IMAGE_LABELS, \
+    TILE_SIZE
+
+
+class EncDecUnpoolNet(nn.Module):
+    """
+    EncDecUnpool network based on VGG16. It is inspired by Deconvnet
+    "Learning Deconvolution Network for Semantic Segmentation", H. Noh et al.
+
+    The network returns two outputs: one for the pixel-wise classification and
+    one for the multiclass image classification
+    """
+
+    @staticmethod
+    def weight_init(m):
+        if isinstance(m, nn.Linear):
+            torch.nn.init.kaiming_normal_(m.weight.data)
+
+    def __init__(self,
+                 in_channels=INPUT_CHANNELS,
+                 pixel_out_channels=N_CLASSES,
+                 image_out_channels=N_CLASSES_IMAGE_LABELS):
+        super(EncDecUnpoolNet, self).__init__()
+        self.pool = nn.MaxPool2d(2, return_indices=True)
+        self.unpool = nn.MaxUnpool2d(2)
+
+        self.conv1_1 = nn.Conv2d(in_channels, 64, 3, padding=1)
+        self.conv1_1_bn = nn.BatchNorm2d(64)
+        self.conv1_2 = nn.Conv2d(64, 64, 3, padding=1)
+        self.conv1_2_bn = nn.BatchNorm2d(64)
+
+        self.conv2_1 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv2_1_bn = nn.BatchNorm2d(128)
+        self.conv2_2 = nn.Conv2d(128, 128, 3, padding=1)
+        self.conv2_2_bn = nn.BatchNorm2d(128)
+
+        self.conv3_1 = nn.Conv2d(128, 256, 3, padding=1)
+        self.conv3_1_bn = nn.BatchNorm2d(256)
+        self.conv3_2 = nn.Conv2d(256, 256, 3, padding=1)
+        self.conv3_2_bn = nn.BatchNorm2d(256)
+        self.conv3_3 = nn.Conv2d(256, 256, 3, padding=1)
+        self.conv3_3_bn = nn.BatchNorm2d(256)
+
+        self.conv4_1 = nn.Conv2d(256, 512, 3, padding=1)
+        self.conv4_1_bn = nn.BatchNorm2d(512)
+        self.conv4_2 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv4_2_bn = nn.BatchNorm2d(512)
+        self.conv4_3 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv4_3_bn = nn.BatchNorm2d(512)
+
+        self.conv5_1 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_1_bn = nn.BatchNorm2d(512)
+        self.conv5_2 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_2_bn = nn.BatchNorm2d(512)
+        self.conv5_3 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_3_bn = nn.BatchNorm2d(512)
+
+        self.conv5_3_D = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_3_D_bn = nn.BatchNorm2d(512)
+        self.conv5_2_D = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_2_D_bn = nn.BatchNorm2d(512)
+        self.conv5_1_D = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_1_D_bn = nn.BatchNorm2d(512)
+
+        self.conv4_3_D = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv4_3_D_bn = nn.BatchNorm2d(512)
+        self.conv4_2_D = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv4_2_D_bn = nn.BatchNorm2d(512)
+        self.conv4_1_D = nn.Conv2d(512, 256, 3, padding=1)
+        self.conv4_1_D_bn = nn.BatchNorm2d(256)
+
+        self.conv3_3_D = nn.Conv2d(256, 256, 3, padding=1)
+        self.conv3_3_D_bn = nn.BatchNorm2d(256)
+        self.conv3_2_D = nn.Conv2d(256, 256, 3, padding=1)
+        self.conv3_2_D_bn = nn.BatchNorm2d(256)
+        self.conv3_1_D = nn.Conv2d(256, 128, 3, padding=1)
+        self.conv3_1_D_bn = nn.BatchNorm2d(128)
+
+        self.conv2_2_D = nn.Conv2d(128, 128, 3, padding=1)
+        self.conv2_2_D_bn = nn.BatchNorm2d(128)
+        self.conv2_1_D = nn.Conv2d(128, 64, 3, padding=1)
+        self.conv2_1_D_bn = nn.BatchNorm2d(64)
+
+        self.conv1_2_D = nn.Conv2d(64, 64, 3, padding=1)
+        self.conv1_2_D_bn = nn.BatchNorm2d(64)
+        self.conv1_1_D = nn.Conv2d(64, pixel_out_channels, 3, padding=1)
+
+        self.flatten = nn.Flatten()
+        self.fully_connected_1 = \
+            nn.Linear(TILE_SIZE[0] * TILE_SIZE[1] * pixel_out_channels,
+                      100)
+        self.fully_connected_2 = \
+            nn.Linear(100, image_out_channels)
+        self.dropout = nn.Dropout(p=0.5)
+        self.apply(self.weight_init)
+
+    def forward(self, x):
+        # Encoder block 1
+        x = self.conv1_1_bn(F.relu(self.conv1_1(x)))
+        x = self.conv1_2_bn(F.relu(self.conv1_2(x)))
+        x, mask1 = self.pool(x)
+        size1 = x.size()
+
+        # Encoder block 2
+        x = self.conv2_1_bn(F.relu(self.conv2_1(x)))
+        x = self.conv2_2_bn(F.relu(self.conv2_2(x)))
+        x, mask2 = self.pool(x)
+        size2 = x.size()
+
+        # Encoder block 3
+        x = self.conv3_1_bn(F.relu(self.conv3_1(x)))
+        x = self.conv3_2_bn(F.relu(self.conv3_2(x)))
+        x = self.conv3_3_bn(F.relu(self.conv3_3(x)))
+        x, mask3 = self.pool(x)
+        size3 = x.size()
+
+        # Encoder block 4
+        x = self.conv4_1_bn(F.relu(self.conv4_1(x)))
+        x = self.conv4_2_bn(F.relu(self.conv4_2(x)))
+        x = self.conv4_3_bn(F.relu(self.conv4_3(x)))
+        x, mask4 = self.pool(x)
+        size4 = x.size()
+
+        # Encoder block 5
+        x = self.conv5_1_bn(F.relu(self.conv5_1(x)))
+        x = self.conv5_2_bn(F.relu(self.conv5_2(x)))
+        x = self.conv5_3_bn(F.relu(self.conv5_3(x)))
+        x, mask5 = self.pool(x)
+
+        # Decoder block 5
+        x = self.unpool(x, mask5, output_size=size4)
+        x = self.conv5_3_D_bn(F.relu(self.conv5_3_D(x)))
+        x = self.conv5_2_D_bn(F.relu(self.conv5_2_D(x)))
+        x = self.conv5_1_D_bn(F.relu(self.conv5_1_D(x)))
+
+        # Decoder block 4
+        x = self.unpool(x, mask4, output_size=size3)
+        x = self.conv4_3_D_bn(F.relu(self.conv4_3_D(x)))
+        x = self.conv4_2_D_bn(F.relu(self.conv4_2_D(x)))
+        x = self.conv4_1_D_bn(F.relu(self.conv4_1_D(x)))
+
+        # Decoder block 3
+        x = self.unpool(x, mask3, output_size=size2)
+        x = self.conv3_3_D_bn(F.relu(self.conv3_3_D(x)))
+        x = self.conv3_2_D_bn(F.relu(self.conv3_2_D(x)))
+        x = self.conv3_1_D_bn(F.relu(self.conv3_1_D(x)))
+
+        # Decoder block 2
+        x = self.unpool(x, mask2, output_size=size1)
+        x = self.conv2_2_D_bn(F.relu(self.conv2_2_D(x)))
+        x = self.conv2_1_D_bn(F.relu(self.conv2_1_D(x)))
+
+        # Decoder block 1
+        x = self.unpool(x, mask1)
+        x = self.conv1_2_D_bn(F.relu(self.conv1_2_D(x)))
+        x = self.dropout(x)
+        x = self.conv1_1_D(x)
+
+        x = F.log_softmax(x)
+        # Flattening
+        y = self.flatten(x)
+        # Fully connected layers
+        y = F.relu(self.fully_connected_1(y))
+        y = self.fully_connected_2(y)
+        return x, y
